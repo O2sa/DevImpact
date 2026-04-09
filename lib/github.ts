efe03994985b34fa1e0f87f41851cdf2d6234a79
Osama Mabkhot <99215291+O2sa@users.noreply.github.com>
2026-04-09 04:07:03 +0300

    feat: Fetch User Core Data

--- lib/github.ts
+++ lib/github.ts
@@ -1,3 +1,4 @@
+import { ContributionTotals, GitHubUserData, PullRequestNode, RepoNode } from "@/types/github";
 import { graphql } from "@octokit/graphql";
 
 if (!process.env.GITHUB_TOKEN) {
@@ -11,3 +12,63 @@ const client = graphql.defaults({
 });
 
 
+const QUERY = /* GraphQL */ `
+  query FetchUserData($login: String!, $repoCount: Int = 100, $prCount: Int = 100) {
+    user(login: $login) {
+      repositories(
+        first: $repoCount
+        privacy: PUBLIC
+        ownerAffiliations: OWNER
+        orderBy: { field: STARGAZERS, direction: DESC }
+      ) {
+        nodes {
+          name
+          stargazerCount
+          forkCount
+          watchers {
+            totalCount
+          }
+        }
+      }
+      pullRequests(
+        first: $prCount
+        states: [MERGED]
+        orderBy: { field: CREATED_AT, direction: DESC }
+      ) {
+        nodes {
+          merged
+          additions
+          deletions
+          repository {
+            nameWithOwner
+            stargazerCount
+            owner {
+              login
+            }
+          }
+        }
+      }
+      contributionsCollection {
+        totalCommitContributions
+        totalPullRequestContributions
+        totalIssueContributions
+      }
+    }
+  }
+`;
+
+export async function fetchGitHubUserData(
+  username: string
+): Promise<GitHubUserData> {
+  const { user } = await client<{ user: any }>(QUERY, { login: username });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return {
+    repos: user.repositories.nodes as RepoNode[],
+    pullRequests: user.pullRequests.nodes as PullRequestNode[],
+    contributions: user.contributionsCollection as ContributionTotals,
+  };
+}
